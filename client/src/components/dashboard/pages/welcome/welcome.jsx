3bc5f10dc5bd3f0b55943123bda4bee061005687
Matt T <code.matthew.thompson@gmail.com>
2017-01-15 14:16:49 -0500

    finishes answer history/paginates it  on welcome screen

--- client/src/components/dashboard/pages/welcome/welcome.jsx
+++ client/src/components/dashboard/pages/welcome/welcome.jsx
@@ -14,6 +14,10 @@ export default class WelcomeUI extends Component {
     this.props._historyActions.getHistory()
   }
 
+  componentWillReceiveProps (props) {
+
+  }
+
   render () {
     return (
       <div>
@@ -24,15 +28,57 @@ export default class WelcomeUI extends Component {
               <Spinner />
             </div>
           </div>
-          : <div style={{textAlign: 'center'}}>
-            Welcome!
-            <br />
-            ***Show answer history.. paginate***
-            <br />
-            {this.props.history.length}
+          : <div style={{height: '100%', textAlign: 'center'}}>
+            -Your answer history-
+            <div>
+              {renderAnswers(
+                this.props.history,
+                this.props.appData.welcome.page)}
+            </div>
+            <div className='page-buttons'>
+              {this.props.appData.welcome.page !== 0
+              ? <div className='pull-left history-button'
+                onClick={() => this.props._appActions.change(
+                  this.props.appData.welcome.page - 1,
+                  'page',
+                  'welcome'
+                )}><i className='fa fa-arrow-left fa-2x' aria-hidden='true' />
+              </div>
+              : null}
+              {this.props.appData.welcome.page <= Math.floor((this.props.history.length / 5) - 1)
+              ? <div className='pull-right history-button'
+                onClick={() => this.props._appActions.change(
+                  this.props.appData.welcome.page + 1,
+                  'page',
+                  'welcome'
+                )}><i className='fa fa-arrow-right fa-2x' aria-hidden='true' />
+              </div>
+              : null}
+            </div>
           </div>
         }
       </div>
     )
   }
+}
+
+function renderAnswers (answers, page) {
+  if (answers.length > 0) {
+    return answers.map((answer, index) => (
+      <Answer key={index} answer={answer} />
+        )).splice(page === 0 ? 0 : page * 5, 5)
+  }
+  else return []
+}
+
+const Answer = ({answer}) => {
+  return (
+    <div className='history-answer'>
+      Question: {answer.question.body}
+      <br />
+      Your answer: {answer.answer}
+      <br />
+      correct?: {answer.correct ? 'true' : 'false'}
+    </div>
+  )
 }
--- client/src/redux/reducers/appData.js
+++ client/src/redux/reducers/appData.js
@@ -7,6 +7,7 @@ const appData = (state, action) => {
         loading: false
       },
       welcome: {
+        page: 0,
         loading: false
       },
       add: {
